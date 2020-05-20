1e011b636c980da409079bb3ee849a8503300617
jeiko <jeisonsma@hotmail.com>
2020-05-19 22:37:15 -0500

    subiendo archivos en github

---
+++ README.md
@@ -0,0 +1,1 @@
+Vendor blobs for huawei Y360
---
+++ vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/huawei/hwy360-u/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/huawei/hwy360-u/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/huawei/hwy360-u/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/huawei/hwy360-u/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/huawei/hwy360-u/proprietary/vendor,system/vendor)
